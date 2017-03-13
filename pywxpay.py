770c7944898f62f2727bc17532675cb10ad8c2af
letiantian <sunlt1699@gmail.com>
2017-03-13 22:29:49 +0800

    fix report url; update to v0.3

--- pywxpay.py
+++ pywxpay.py
@@ -82,7 +82,7 @@ class WXPayConstants(object):
     REFUND_URL = "https://api.mch.weixin.qq.com/secapi/pay/refund"
     REFUNDQUERY_URL = "https://api.mch.weixin.qq.com/pay/refundquery"
     DOWNLOADBILL_URL = "https://api.mch.weixin.qq.com/pay/downloadbill"
-    REPORT_URL = "https://api.mch.weixin.qq.com/pay/report"
+    REPORT_URL = "https://api.mch.weixin.qq.com/payitil/report"
     SHORTURL_URL = "https://api.mch.weixin.qq.com/tools/shorturl"
     AUTHCODETOOPENID_URL = "https://api.mch.weixin.qq.com/tools/authcodetoopenid"
 
@@ -95,7 +95,7 @@ class WXPayConstants(object):
     SANDBOX_REFUND_URL = "https://api.mch.weixin.qq.com/sandboxnew/secapi/pay/refund"
     SANDBOX_REFUNDQUERY_URL = "https://api.mch.weixin.qq.com/sandboxnew/pay/refundquery"
     SANDBOX_DOWNLOADBILL_URL = "https://api.mch.weixin.qq.com/sandboxnew/pay/downloadbill"
-    SANDBOX_REPORT_URL = "https://api.mch.weixin.qq.com/sandboxnew/pay/report"
+    SANDBOX_REPORT_URL = "https://api.mch.weixin.qq.com/sandboxnew/payitil/report"
     SANDBOX_SHORTURL_URL = "https://api.mch.weixin.qq.com/sandboxnew/tools/shorturl"
     SANDBOX_AUTHCODETOOPENID_URL = "https://api.mch.weixin.qq.com/sandboxnew/tools/authcodetoopenid"
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pywxpay',
-    version='0.2',
+    version='0.3',
     description='python wxpay sdk.',
     long_description=open('README.rst').read(),
     author='wxpay',
